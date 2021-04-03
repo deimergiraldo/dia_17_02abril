e157ef89c9a1ef6026e947233152e0344db093fb
Deimer Estiven Estiven <>
2021-04-03 06:04:31 +0000

    he creado escrito en las casillas y he creado nuevas hojas en el mismo archivo

--- main.py
+++ main.py
@@ -4,19 +4,32 @@ import openpyxl
 #Crea un archivo de excel
 deimer= openpyxl.Workbook()
 
+hojad = deimer.active
 
-#Vamos a cambiar el nombre de la hoja
-#bailes.titles('NuevosBailes')
+# Voy a escribir en la hoja de cálculo
+hojad['A1'] = "Canciones"
+hojad['A2'] = "Vamos a cantar"
+hojad['B1'] = "Género"
+hojad['B2'] = "¿Qué canción?"
+hojad['C1'] = "Año"
+hojad['C2'] ="Pues yo no sé, dime tú"
+hojad['D1'] = "¿Es famosa la canción?"
+hojad['D2'] = "Ok!... entonces cantaremos los pollitos"
 
 deimer.save('excel_deimer.xlsx')
 
+#Ahora vamos a leer el archivos
 from openpyxl import load_workbook
 
-dei= load_workbook('excel_deimer.xlsx')
+deimer = openpyxl.load_workbook('excel_deimer.xlsx')
+deimer= load_workbook('excel_deimer.xlsx')
 
-#Crea varias hojas de calculo en el mismo archivo de excel
-canciones=dei.create_sheet('Hoja1')
-bailes=dei.create_sheet('Hoja2',0)
-discotecas=dei.create_sheet('Hoja3',-1)
+#Crea otra hoja de calculo en el mismo archivo de excel
+canciones=deimer.create_sheet('Hoja1')
+bailes=deimer.create_sheet('Hoja2',0)
+discotecas=deimer.create_sheet('Hoja3',-1)
+ 
+#Vamos a cambiar el nombre de la hoja
+#canciones.titles('NuevasCanciones')
 
-dei.save('excel.deimer.xlsx')
+deimer.save('excel_deimer.xlsx')
